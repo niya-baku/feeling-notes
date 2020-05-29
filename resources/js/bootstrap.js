0de97553885105100de9a6e978fa03acb6e9f640
vagrant <vagrant@homestead>
2020-05-29 11:48:49 +0000

    heroku公開用コード修正16

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,7 +3,17 @@ import { getCookieValue } from './util'
 window.axios = require('axios')
 
 // Ajaxリクエストであることを示すヘッダーを付与する
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+
+if (token) {
+  //window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+  window.axios.defaults.headers.common = {
+      'Accept':'application/json',
+      'Content-Type':'application/json'
+  };
+} else {
+  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 window.axios.interceptors.request.use(config => {
   // クッキーからトークンを取り出してヘッダーに添付する
